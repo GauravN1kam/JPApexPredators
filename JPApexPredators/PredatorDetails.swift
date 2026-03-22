a0337c2d989721c7c4a0a0e880c9d2f75dc8fa39
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-03-22 19:45:39 +0530

    completed contentview

---
+++ JPApexPredators/PredatorDetails.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetails.swift
+//  JPApexPredators
+//
+//  Created by Gaurav Nikam on 22/03/26.
+//
+
+import SwiftUI
+
+struct PredatorDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetails()
+}
